fc5c3c63876b00d792d88401565317189ac9fe39
Justin Gallardo <justin.gallardo@conductorone.com>
2023-05-05 17:52:37 -0700

    ConductorOne/baton-linear => conductorone/baton-linear

--- pkg/connector/team.go
+++ pkg/connector/team.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/ConductorOne/baton-linear/pkg/linear"
+	"github.com/conductorone/baton-linear/pkg/linear"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/pagination"
--- pkg/connector/user.go
+++ pkg/connector/user.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/ConductorOne/baton-linear/pkg/linear"
+	"github.com/conductorone/baton-linear/pkg/linear"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/pagination"
